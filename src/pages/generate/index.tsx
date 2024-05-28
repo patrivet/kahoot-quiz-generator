ffc7067b0c0990e8379c86fff29884bcce8c18be
Pat Rivet <patrickmrivet@gmail.com>
2024-05-28 09:03:38 +0100

    wip

--- src/pages/generate/index.tsx
+++ src/pages/generate/index.tsx
@@ -1,7 +1,77 @@
 import { NextPage } from 'next'
+import { useState } from 'react'
 
-const ViewQuizzes: NextPage = () => {
-  return <h1>GENERATOR PAGE</h1>
+const GenerateQuiz: NextPage = () => {
+  const [topping, setTopping] = useState("Medium")
+
+  const onOptionChange = e => {
+    setTopping(e.target.value)
+  }
+
+  // return <h1>GENERATOR PAGE</h1>
+  return (
+    <div className="App">
+      <fieldset>
+        <legend>How many questions to generate?:</legend>
+        <input
+          type="radio"
+          name="topping"
+          value="Regular"
+          id="regular"
+          checked={topping === "Regular"}
+          onChange={onOptionChange}
+        />
+        <label htmlFor="regular">Regular</label>
+
+        <input
+          type="radio"
+          name="topping"
+          value="Medium"
+          id="medium"
+          checked={topping === "Medium"}
+          onChange={onOptionChange}
+        />
+        <label htmlFor="medium">Medium</label>
+
+        <input
+          type="radio"
+          name="topping"
+          value="Large"
+          id="large"
+          checked={topping === "Large"}
+          onChange={onOptionChange}
+        />
+        <label htmlFor="large">Large</label>
+      </fieldset>
+
+      <p>Selected <strong>{topping}</strong> questions</p>
+    </div>
+  )
 }
 
-export default ViewQuizzes
+export default GenerateQuiz
+
+
+
+/* doesn't work below:
+
+  //     {/* <form onSubmit={formSubmit}> */
+  //       {/* <fieldset>
+  //         <legend>Select number of questions:</legend> */}
+  //         <div>
+  //           <input type="radio" id="20" name="drone" value="20" onChange={onOptionChange} checked={numQuestions === '20'} />
+  //           <label htmlFor="20">20</label>
+  //         </div>
+  //         <div>
+  //           <input type="radio" id="30" name="drone" onChange={onOptionChange} value="30" checked={numQuestions === '30'}  />
+  //           <label htmlFor="30">30</label>
+  //         </div>
+  //         <div>
+  //           <input type="radio" id="40" name="drone" onChange={onOptionChange} value="40"  checked={numQuestions === '40'} />
+  //           <label htmlFor="40">40</label>
+  //         </div>
+  //         <div>
+  //           <input type="radio" id="Custom" name="drone" onChange={onOptionChange} value="Custom"  checked={numQuestions === 'Custom'} />
+  //           <label id="Custom">Custom</label>
+  //         </div>
+  //       {/* </fieldset> */}
--- src/pages/view/index.tsx
+++ src/pages/view/index.tsx
@@ -1,7 +1,28 @@
 import { NextPage } from 'next'
 
-const GenerateQuiz: NextPage = () => {
+const ViewQuizzes: NextPage = () => {
+
+/* testing API */
+// type ApiQuestionType = {
+  //   question: {
+    //     id: string;
+    //     text: string
+    //   }
+    // }
+    // const [apiQs, setApiQs] = React.useState<ApiQuestionType[]>()
+    // useEffect(() => {
+      //   console.log(`Running useEffect() - fetching trivia questions..`)
+      //   let res
+      //   (async () => {
+        //     res = await fetch('/api/trivia?categories=film_and_tv')
+        //     res = await res.json()
+        //     console.log(`Fetched trivia questions =`, res)
+        //   })()
+        // }, [])
+        // console.log("🚀 ~ file: View renders")
+
+
   return <h1>VIEW QUIZZES</h1>
 }
 
-export default GenerateQuiz
+export default ViewQuizzes
